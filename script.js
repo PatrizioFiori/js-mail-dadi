b11db162bb3f504fc2fca061e477c7ccb40334ba
PatrizioFiori <patrizio.fiori1995@gmail.com>
2024-11-18 14:30:28 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Mail: 
+Creo un'array con varie mail che possono accedere. Tramite una if all'interno di un ciclo controllo se la mail che verrà fornita dall'utente può accedere o meno e stampo il conseguente output
+*/
+
+
+let mail = ["mario@gmail.com", "marco@gmail.com", "luca@gmail.com", "paolo@gmail.com" , "matteo@gmail.com"]
